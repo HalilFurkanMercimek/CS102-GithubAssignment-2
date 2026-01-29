336441ecb93af35aae1a1c82458f0f8fb08706eb
Furkan <halilfurkanm@gmail.com>
2026-01-29 09:36:27 +0300

    created method outlines

--- main.java
+++ main.java
@@ -1,1 +1,25 @@
+public class main {
 
+    public static void main(String[] args) {
+        
+    }
+
+    public int[] createArray(int size){
+
+    }
+    public void printMenu(){
+            
+    }
+    public void findMax(int[] arr){
+
+    }
+     public void findMin(int[] arr){
+
+    }
+    public void partD(){
+
+    }
+    public void partE(){
+        
+    }
+}
