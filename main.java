cd12b478a33cb24ecccd5850e0b3f544876ec093
Selman Gökçe <selmangokce0705@hotmail.com>
2026-01-29 10:00:31 +0300

    part E redesign

--- main.java
+++ main.java
@@ -19,7 +19,20 @@ public class main {
     public void partD(){
 
     }
-    public void partE(){
+    public void findSumOfElements(int[] arr){
+        int length = arr.length;
+        int sumOfOdd = 0, sumOfEven = 0;
         
+        for(int i = 0; i < length; i++){
+            if(i % 2 == 0){
+                sumOfEven += arr[i];
+            }
+            else{
+                sumOfOdd += arr[i];
+            }
+        }
+
+        System.out.println("Sum of odd indexes is " + sumOfOdd);
+        System.out.println("Sum of even indexes is " + sumOfEven);
     }
 }
